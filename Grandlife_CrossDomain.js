fb129b422776ef63dae8b90249394ca8ac4c2e81
Jknox86 <jordan@knoxim.com>
2016-02-05 17:47:09 -0500

    Added Grandlife

---
+++ Grandlife_CrossDomain.js
@@ -0,0 +1,38 @@
+
+
+//This is for the main domain grandlifehotels
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  	ga('create', 'UA-2223906-4', 'auto', {'allowLinker':true}};
+	ga('require', 'linker');
+	ga('linkger:autoLink',['roxyhotelnyc.com', 'sohogrand.com']);
+  	ga('send', 'pageview');
+
+ga('create', 'UA-2223906-4', 'auto', {'allowLinker':true}};
+ga('require', 'linker');
+ga('linker:autoLink',['roxyhotelnyc.com', 'sohogrand.com']);
+
+
+//This is for the main domain grandlifehotels
+
+ga('create', 'UA-2223906-4', 'auto', {'allowLinker':true}};
+ga('require', 'linker');
+ga('linker:autoLink',['roxyhotelnyc.com', 'sohogrand.com']);
+
+// these are for secondary domain roxy hotel
+
+
+ga('create', 'UA-2223906-4', 'auto', {'allowLinker':true}};
+ga('require', 'linker');
+ga('linker:autoLink',['grandlifehotels.com', 'sohogrand.com']);
+
+//final domain code snippet for sohogrand hotel
+
+ga('create', 'UA-2223906-4', 'auto', {'allowLinker':true}};
+ga('require', 'linker');
+ga('linker:autoLink',['grandlifehotels.com', 'roxyhotelnyc.com']);
+
